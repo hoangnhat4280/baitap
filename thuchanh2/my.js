e66f4e779e0772b93c825c625bce8e79b0f4c215
hoangnhat4280 <nhatduong3002@gmail.com>
2024-09-13 09:26:41 +0700

    hello

---
+++ thuchanh2/my.js
@@ -0,0 +1,12 @@
+let bmi = weight / (height * 2);
+
+
+if (bmi < 18,5) {
+    document.write("Underweight");
+} else if (bmi <= 18,5 ) {
+    document.write("Normal");
+} else if (bmi <= 25.0) {
+    document.write("Over weight");
+} else {
+    document.write("Obese");
+}
